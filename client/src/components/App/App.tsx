0637f25f6c25e5242687e923afa668683bb126ed
jannkasper <jann.kasper@gmail.com>
2021-05-20 07:45:46 +0200

    client | fetch temporary

--- client/src/components/App/App.tsx
+++ client/src/components/App/App.tsx
@@ -1,13 +1,26 @@
-import React, {FC} from 'react';
+import React, { useEffect } from 'react';
 import styled from '@emotion/styled';
-import { css } from '@emotion/react';
 import Home from "../Home";
-import Sidebar from "../Sidebar/Sidebar";
+import Sidebar from "../Sidebar";
 import { AppProps } from "./index";
 
-const App: React.FC<AppProps> = ({ showPopup, hasSelectedConversation, userExtId, receiveMessage, receiveNewContact, isLoading, completed }) => {
+const App: React.FC<AppProps> = ({
+    showPopup,
+    hasSelectedConversation,
+    userExtId, receiveMessage,
+    receiveNewContact,
+    isLoading,
+    completed,
+    fetchContacts,
+    fetchConversations
+}) => {
+    useEffect(() => {
+        fetchContacts(userExtId);
+        fetchConversations(userExtId);
+    }, [userExtId]);
+
     return (
-        <AppWrapper>
+        <AppWrapper isBlur={showPopup} zoomOut={!showPopup}>
             <InnerWrapper>
                 <Sidebar/>
                 <Home/>
@@ -16,43 +29,32 @@ const App: React.FC<AppProps> = ({ showPopup, hasSelectedConversation, userExtId
     );
 }
 
-const AppWrapper = styled("div")`
+type AppWrapperProps = {
+    isBlur: boolean
+    zoomOut: boolean
+}
+const AppWrapper = styled("div")<AppWrapperProps>`
   position: relative;
   display: flex;
   margin: 0 auto;
   width: 100%;
   height: 100%;
-  @media (min-width: 1440px) {
-      top: 19px;
-      width: 1400px;
-      height: calc(100% - 38px);
+  filter: ${props => props.isBlur ? 'blur(4px)' : 'unset'};
+  ${props => props.zoomOut && `
+    animation: zoomInOutPage 200ms ease-in;
+    
+    @keyframes zoomInOutPage {
+    0% { opacity: 0; transform:scale(1.5)}
+    100% { opacity: 1; transform:scale(1)}
+    }
+  `};
+
+  @media (min-width: 1440px) {  
+    top: 19px;
+    width: 1400px;
+    height: calc(100% - 38px);  
   }
 `
-
-// const blurStyle = css`
-//   filter: blur(4px);
-// `
-//
-// const zoomOutStyle = css`
-//   animation: zoomInOutPage 200ms ease-in;
-//
-//   @keyframes zoomInOutPage {
-//     0% { opacity: 0; transform:scale(1.5)}
-//     100% { opacity: 1; transform:scale(1)}
-//   }
-// `
-//
-// interface ComponentProps {
-//     className?: string,
-//     showPopup?: boolean,
-//     children?: any
-// }
-//
-// const Component: FC<ComponentProps> = ({
-//    showPopup,
-//    className,
-//     children
-// }) => <div css={{background: 'black'}} className={className} >{children}</div>
 
 const InnerWrapper = styled("div")`
   position: relative;
--- client/src/components/App/index.tsx
+++ client/src/components/App/index.tsx
@@ -2,17 +2,23 @@ import { connect, ConnectedProps } from "react-redux";
 import App from "./App";
 import { ApplicationState } from "../../reducers";
 import { receiveMessage, receiveNewContact } from "../../actions";
+import fetchContacts from "../../store/fetchContacts";
+import fetchConversations from "../../store/fetchConversations";
 
 const mapStateToProps = (state: ApplicationState) => ({
     showPopup: !Boolean(state.user.externalIdentifier),
-    userExtId: state.user.externalIdentifier,
+    userExtId: 'Q1u4T6sZ2' || state.user.externalIdentifier,
     hasSelectedConversation: Boolean(state.conversations.selectedConversation),
     isLoading: state.contacts.pending || state.conversations.pending,
     completed: Number(state.contacts.pending) + Number(state.conversations.pending) / 2 * 100
 })
 const mapDispatchToProps = {
     receiveMessage,
-    receiveNewContact
+    receiveNewContact,
+
+    //temporary
+    fetchContacts,
+    fetchConversations
 }
 
 const connector = connect(mapStateToProps, mapDispatchToProps)
